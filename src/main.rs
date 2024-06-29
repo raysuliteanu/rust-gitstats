39ed1dc18d6644072f5b20ede2d0d4976648cff6
Ray Suliteanu <raysuliteanu@gmail.com>
2024-06-29 16:53:31 -0700

    start collecting commits by person

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+use std::collections::HashMap;
 use std::error::Error;
+use std::fmt::{Display, Formatter};
 
 use anyhow::Result;
 use git2::Repository;
+
+struct Person {
+    name: String,
+    email: String,
+    commits: usize,
+}
+
+impl Person {
+    fn new(name: String, email: String, commits: usize) -> Self {
+        Person {
+            name,
+            email,
+            commits,
+        }
+    }
+}
+
+impl Display for Person {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("Name: {} ({}) Commits: {}", self.name, self.email, self.commits))
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // TODO: optional path from CLI args
@@ -11,24 +35,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     revwalk.push_head()?;
 
-    macro_rules! filter_try {
-        ($e:expr) => {
-            match $e {
-                Ok(t) => t,
-                Err(e) => return Some(Err(e)),
-            }
-        };
-    }
-    let revwalk = revwalk
-        .filter_map(|id| {
-            let id = filter_try!(id);
-            let commit = filter_try!(repo.find_commit(id));
-            //        let parents = commit.parents().len();
-            Some(Ok(commit))
-        })
-        .take(10);
+    let mut people: HashMap<String, Person> = HashMap::new();
 
-    revwalk.for_each(|c| println!("{:?}", c));
+    let _commits: Vec<_> = revwalk
+        .filter_map(|id| id.ok())
+        .filter_map(|commit_id| repo.find_commit(commit_id).ok())
+        .map(|commit| {
+            println!("{:?}", commit);
+            let signature = commit.committer();
+            let committer_name = signature.name().unwrap();
+            let committer_email = signature.email().unwrap();
+            let entry = people.entry(committer_name.to_string())
+                .or_insert(Person::new(committer_name.to_string(), committer_email.to_string(), 0));
+            entry.commits += 1;
+        })
+        .collect();
+
+    people.into_values()
+        .for_each(|person| println!("{person}"));
 
     Ok(())
 }
