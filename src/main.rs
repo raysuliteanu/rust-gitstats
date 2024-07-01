4c748b28afffe2dd75b95683d342649acec36529
Ray Suliteanu <raysuliteanu@gmail.com>
2024-07-01 13:00:32 -0700

    reworked to collect stats

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,118 @@
 use std::collections::HashMap;
 use std::error::Error;
-use std::fmt::{Display, Formatter};
+use std::hash::Hash;
+use std::path::PathBuf;
 
 use anyhow::Result;
-use git2::Repository;
+use clap::Parser;
+use git2::{BranchType, Repository, Status};
 
-struct Person {
-    name: String,
-    email: String,
-    commits: usize,
+#[derive(Default, Debug)]
+struct GitStats {
+    commits_by_contributor: HashMap<String, usize>,
+    local_branches: usize,
+    remote_branches: usize,
+    _tags: usize,
+    files_by_status: HashMap<StatusMapping, usize>,
+}
+// see git_status_t from git2::StatusEntry; it's not hashable so need to map
+#[derive(Hash, Ord, Eq, Debug, PartialOrd, PartialEq)]
+enum StatusMapping {
+    Current ,
+    IndexNew ,
+    IndexModified ,
+    IndexDeleted ,
+    IndexRenamed ,
+    IndexTypeChange ,
+    WtNew ,
+    WtModified ,
+    WtDeleted ,
+    WtTypeChange ,
+    WtRenamed ,
+    WtUnreadable ,
+    Ignored ,
+    Conflicted ,
 }
 
-impl Person {
-    fn new(name: String, email: String, commits: usize) -> Self {
-        Person {
-            name,
-            email,
-            commits,
+// todo: this won't work :P ... rev can be in multiple states at the same time;
+// they're just bit flags and so status could be 'WT_MODIFIED|CONFLICTED' or some such
+impl From<Status> for StatusMapping {
+    fn from(status: Status) -> Self {
+        match status {
+            Status::CURRENT => StatusMapping::Current,
+            Status::INDEX_NEW => StatusMapping::IndexNew,
+            Status::INDEX_MODIFIED => StatusMapping::IndexModified,
+            Status::INDEX_DELETED => StatusMapping::IndexDeleted,
+            Status::INDEX_RENAMED => StatusMapping::IndexRenamed,
+            Status::INDEX_TYPECHANGE => StatusMapping::IndexTypeChange,
+            Status::WT_NEW => StatusMapping::WtNew,
+            Status::WT_MODIFIED => StatusMapping::WtModified,
+            Status::WT_DELETED => StatusMapping::WtDeleted,
+            Status::WT_RENAMED => StatusMapping::WtRenamed,
+            Status::WT_TYPECHANGE => StatusMapping::WtTypeChange,
+            Status::IGNORED => StatusMapping::Ignored,
+            Status::CONFLICTED => StatusMapping::Conflicted,
+            _ => {
+                eprintln!("unexpected revision status {:?}", status);
+                StatusMapping::WtUnreadable
+            }
         }
     }
 }
 
-impl Display for Person {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!(
-            "Name: {} ({}) Commits: {}",
-            self.name, self.email, self.commits
-        ))
-    }
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Options {
+    /// The path to the Git repo
+    #[arg(short, long, value_name = "DIR", default_value = ".")]
+    repo_dir: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     // TODO: optional path from CLI args
-    let path = ".";
-    let repo = Repository::open(path)?;
-    let mut revwalk = repo.revwalk()?;
+    let options = Options::parse();
 
+    let path = options.repo_dir;
+    let repo = Repository::open(path)?;
+    println!("Repository state: {:?}", repo.state());
+
+    let mut stats = GitStats::default();
+
+    let branches = repo.branches(None)?;
+    branches
+        .filter_map(|res| res.ok())
+        .map(|(_, branch_type)| match branch_type {
+            BranchType::Local => stats.local_branches += 1,
+            BranchType::Remote => stats.remote_branches += 1,
+        })
+        .count();
+
+    let statuses = repo.statuses(None)?;
+    statuses
+        .iter()
+        .map(|status| {
+            let s = StatusMapping::from(status.status());
+            let value = stats.files_by_status.entry(s).or_insert(0);
+            *value += 1;
+        })
+        .count();
+
+    let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
 
-    let mut people: HashMap<String, Person> = HashMap::new();
-
-    let _commits: Vec<_> = revwalk
+    revwalk
         .filter_map(|id| id.ok())
         .filter_map(|commit_id| repo.find_commit(commit_id).ok())
-        .map(|commit| {
-            let signature = commit.committer();
-            let committer_name = signature.name().unwrap();
-            let committer_email = signature.email().unwrap();
-            let entry = people
-                .entry(committer_name.to_string())
-                .or_insert(Person::new(
-                    committer_name.to_string(),
-                    committer_email.to_string(),
-                    0,
-                ));
-            entry.commits += 1;
+        .map(|c| c.committer().name().unwrap().to_string())
+        .map(|committer_name| {
+            let value = stats
+                .commits_by_contributor
+                .entry(committer_name)
+                .or_insert(0);
+            *value += 1;
         })
-        .collect();
+        .count();
 
-    people.into_values().for_each(|person| println!("{person}"));
-
+    println!("{:?}", stats);
     Ok(())
 }
