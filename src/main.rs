cd5ee0afcee59912fed5e6432dbbf301da35327f
Ray Suliteanu <raysuliteanu@gmail.com>
2024-07-03 20:37:57 -0700

    remove the file stats as currently implemented
    the current file approach wasn't right, so remove it and implement Display for the
    stats

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,32 @@
 use std::collections::HashMap;
 use std::error::Error;
-use std::hash::Hash;
+use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
-use git2::{BranchType, Repository, Status};
+use git2::{BranchType, Repository};
 
 #[derive(Default, Debug)]
 struct GitStats {
     commits_by_contributor: HashMap<String, usize>,
     local_branches: usize,
     remote_branches: usize,
-    _tags: usize,
-    files_by_status: HashMap<StatusMapping, usize>,
-}
-// see git_status_t from git2::StatusEntry; it's not hashable so need to map
-#[derive(Hash, Ord, Eq, Debug, PartialOrd, PartialEq)]
-enum StatusMapping {
-    Current ,
-    IndexNew ,
-    IndexModified ,
-    IndexDeleted ,
-    IndexRenamed ,
-    IndexTypeChange ,
-    WtNew ,
-    WtModified ,
-    WtDeleted ,
-    WtTypeChange ,
-    WtRenamed ,
-    WtUnreadable ,
-    Ignored ,
-    Conflicted ,
+    tags: usize,
+    _files: usize,
 }
 
-// todo: this won't work :P ... rev can be in multiple states at the same time;
-// they're just bit flags and so status could be 'WT_MODIFIED|CONFLICTED' or some such
-impl From<Status> for StatusMapping {
-    fn from(status: Status) -> Self {
-        match status {
-            Status::CURRENT => StatusMapping::Current,
-            Status::INDEX_NEW => StatusMapping::IndexNew,
-            Status::INDEX_MODIFIED => StatusMapping::IndexModified,
-            Status::INDEX_DELETED => StatusMapping::IndexDeleted,
-            Status::INDEX_RENAMED => StatusMapping::IndexRenamed,
-            Status::INDEX_TYPECHANGE => StatusMapping::IndexTypeChange,
-            Status::WT_NEW => StatusMapping::WtNew,
-            Status::WT_MODIFIED => StatusMapping::WtModified,
-            Status::WT_DELETED => StatusMapping::WtDeleted,
-            Status::WT_RENAMED => StatusMapping::WtRenamed,
-            Status::WT_TYPECHANGE => StatusMapping::WtTypeChange,
-            Status::IGNORED => StatusMapping::Ignored,
-            Status::CONFLICTED => StatusMapping::Conflicted,
-            _ => {
-                eprintln!("unexpected revision status {:?}", status);
-                StatusMapping::WtUnreadable
-            }
-        }
+impl Display for GitStats {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut commits = String::new();
+        self.commits_by_contributor
+            .iter()
+            .for_each(|e| commits.push_str(format!("{}: {}\n", e.0, e.1).as_str()));
+
+        f.write_fmt(format_args!(
+            "{commits}\nBranches:\n\tLocal: {}\n\tRemote: {}\ntags: {}",
+            self.local_branches, self.remote_branches, self.tags
+        ))
     }
 }
 
@@ -69,7 +39,6 @@ struct Options {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO: optional path from CLI args
     let options = Options::parse();
 
     let path = options.repo_dir;
@@ -87,15 +56,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .count();
 
-    let statuses = repo.statuses(None)?;
-    statuses
-        .iter()
-        .map(|status| {
-            let s = StatusMapping::from(status.status());
-            let value = stats.files_by_status.entry(s).or_insert(0);
-            *value += 1;
-        })
-        .count();
+    stats.tags = repo.tag_names(None).map_or(0, |t| t.len());
 
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
@@ -103,7 +64,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     revwalk
         .filter_map(|id| id.ok())
         .filter_map(|commit_id| repo.find_commit(commit_id).ok())
-        .map(|c| c.committer().name().unwrap().to_string())
+        .map(|c| {
+            /*if let Ok(tree) = c.tree() {
+
+            }*/
+            c.committer().name().unwrap().to_string()
+        })
         .map(|committer_name| {
             let value = stats
                 .commits_by_contributor
@@ -113,6 +79,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .count();
 
-    println!("{:?}", stats);
+    println!("{stats}");
     Ok(())
 }
